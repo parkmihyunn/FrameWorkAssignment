7200c4e7edb22021a13a5f1f24a5651205210e32
ParkMi-Hyun <parkmihyunn@gmail.com>
2025-01-09 18:42:45 +0900

    장바구니 링크 수정

--- src/components/carts/CartItem.tsx
+++ src/components/carts/CartItem.tsx
@@ -21,14 +21,14 @@ export default function CartItem({ id, count }) {
       <div className="lg:flex justify-between mb-20">
         <div>
           <div className="lg:flex lg:items-center mt-4 px-2 lg:px-0">
-            <a href="/product/1">
+            <a href={"/product/" + item.id}>
               <figure className="w-56 min-w-full flex-shrink-0 rounded-2xl overflow-hidden px-4 py-4 bg-white">
                 <img src={item.image} alt={item.title} className="object-contain w-full h-48" />
               </figure>
             </a>
             <div className="card-body px-1 lg:px-12">
               <h2 className="card-title">
-                <a className="link link-hover" href="/product/1">
+                <a className="link link-hover" href={"/product/" + item.id}>
                   {item.title}
                 </a>
               </h2>
